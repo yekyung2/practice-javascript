cb7c52fd99788a21a35b8ce135e169bac371d1d6
deepdiver <time0909@g.skku.edu>
2020-09-04 20:39:59 +0900

    learn console.dir() in vanila js

--- nomad.js
+++ nomad.js
@@ -1,3 +1,13 @@
 const title = document.getElementById("title");
 title.innerHTML= "Hi! From JS";
-console.log(title);
+// console.log(title);
+
+console.dir(title); //콘솔창에 h1#title이 뜨고, 많은 attributes가 뜬다! 이렇게 다양한 속성값
+//확인해주고 아래처럼 html이 아닌 js에서 다양한 속성값들을 수정가능 하게된다.
+title.style.color = "royalblue";
+
+console.dir(document); //콘솔창에 #document에 관련된 attributes 뜬다. 여기서 이름을 보고 수정가능!
+document.title = "Scuba Dive-In"
+
+
+ 
