f5d86862e7af88439b59f4225931b7218886f3e5
deepdiver <time0909@g.skku.edu>
2020-10-07 16:56:40 +0900

    add preventDefault

--- greeting.js
+++ greeting.js
@@ -5,17 +5,26 @@ const form = document.querySelector(".js-form"),
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
-    
+
+function handleSubmit(event){
+    event.preventDefault();
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit)
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `Hello ${text}!`;
 }
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-        
+        askForName();
     }else{
         paintGreeting(currentUser);
     }
