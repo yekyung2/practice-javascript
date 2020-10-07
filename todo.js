07bc491530b5c8e9b5b1fd36ab49f4b960eb7f3e
deepdiver <time0909@g.skku.edu>
2020-10-07 17:25:44 +0900

    add todo list basic setting

---
+++ todo.js
@@ -0,0 +1,28 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    to = docuDoListment.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+function paintToDo(text){
+    console.log(text)
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+}
+
+function loadToDos(){
+    const toDos = localStorage.getItem(TODOS_LS);
+    if(toDos !== null){
+
+    }
+}
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit)
+}
+
+init();
