0b05b3075ebf668c272df7e4e32baba19a721bee
deepdiver <time0909@g.skku.edu>
2020-10-07 17:40:28 +0900

    add todo list with x btn

--- todo.js
+++ todo.js
@@ -1,17 +1,26 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
-    to = docuDoListment.querySelector(".js-toDoList");
+    toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
 function paintToDo(text){
-    console.log(text)
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = "✓";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+
 }
 
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
+    toDoInput.value = "";    
 }
 
 function loadToDos(){
